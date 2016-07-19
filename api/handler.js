ac0d011ce6a00a3227bd2864a71a100819ec0d50
Shubham Oli <oli.shubham@gmail.com>
2016-07-19 10:57:35 +0530

    Commit 2: Support for accounts endpoint added

--- api/handler.js
+++ api/handler.js
@@ -6,5 +6,28 @@ var request = require('request');
 
 module.exports = {
 
+  make_get_request :  function(endpoint, token, callback){
+                        var options = {
+                          url: 'https://api.digitalocean.com/v2'+endpoint,
+                          method: 'GET',
+                          headers: {
+                            'Content-Type':'application/json',
+                            'Authorization':'Bearer '+token,
+                          }
+                        };
+
+                        request(options, function(error, response, body){
+                          if(error) {
+                            callback(error, null);
+                          }
+                          else if(response.statusCode !== 200 ){
+                            var err = "Invalid status code received: "+response.statusCode;
+                            callback(err, null);
+                          }
+                          else {
+                            callback(null, body);
+                          }
+                    });
+  }
 
 };
